99b407d0e118fc1c324d2bd437b70e1eab2f14f0
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-02-24 13:03:23 -0600

    updates build script

--- frontend/esbuild.js
+++ frontend/esbuild.js
@@ -4,7 +4,7 @@ import svelte from "esbuild-svelte";
 import postcss from "esbuild-postcss";
 import preprocess from "svelte-preprocess";
 
-const ctx = await esbuild.context({
+const config = {
   entryPoints: [`src/app.ts`],
   bundle: true,
   outdir: `./dist`,
@@ -15,29 +15,37 @@ const ctx = await esbuild.context({
   write: true,
   format: `esm`,
   plugins: [postcss(), svelte({ preprocess: preprocess() })],
-});
+};
 
-const { host, port } = await ctx.serve({ servedir: "./dist" });
+if (!process.argv.includes(`--watch`)) {
+  await esbuild.build(config).catch((err, location) => {
+    console.error(err, location);
+    process.exit(1);
+  });
+} else {
+  const ctx = await esbuild.context(config);
+  const { host, port } = await ctx.serve({ servedir: "./dist" });
 
-http
-  .createServer((req, res) => {
-    const opts = {
-      port: port,
-      path: req.url,
-      hostname: host,
-      method: req.method,
-      headers: req.headers,
-    };
+  http
+    .createServer((req, res) => {
+      const opts = {
+        port: port,
+        path: req.url,
+        hostname: host,
+        method: req.method,
+        headers: req.headers,
+      };
 
-    const proxy = http.request(opts, (proxyRes) => {
-      if (proxyRes.statusCode === 404) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("<h1>404</h1>");
-      }
-      res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    });
+      const proxy = http.request(opts, (proxyRes) => {
+        if (proxyRes.statusCode === 404) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("<h1>404</h1>");
+        }
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        proxyRes.pipe(res, { end: true });
+      });
 
-    req.pipe(proxy, { end: true });
-  })
-  .listen(20231);
+      req.pipe(proxy, { end: true });
+    })
+    .listen(20231);
+}
