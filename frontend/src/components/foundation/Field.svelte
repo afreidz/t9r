be84d98d035f09be9291aeb852754d5a682e00c8
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-04-06 10:15:26 -0500

    back to pseudomono for labels

--- frontend/src/components/foundation/Field.svelte
+++ frontend/src/components/foundation/Field.svelte
@@ -10,7 +10,9 @@
   class={`${$$props.class} my-1 mb-2 flex rounded-md border border-neutral-900 bg-neutral-700 p-4 pt-2 ring-blue-500 ring-offset-2 ring-offset-neutral-800 focus-within:ring-2`}
 >
   <div class="flex flex-1 flex-col">
-    <Copy dim light as="small" class="my-1 font-light">{label || ""}</Copy>
+    <Copy dim light as="small" variant="pseudomono" class="my-1 font-light"
+      >{label || ""}</Copy
+    >
     <slot />
   </div>
   <div class="mt-8 flex flex-none items-start">
