9066825b559606865fa6185bf8f145509b70724f
Andy Freidenfelds <afreidenfelds@gmail.com>
2023-06-02 11:41:07 -0500

    handle redirects from api to login

--- frontend/src/lib/trpc.ts
+++ frontend/src/lib/trpc.ts
@@ -1,6 +1,7 @@
 import { globalError } from "@/lib/stores/ui";
 import type { AppRouter } from "@/backend/trpc/app";
 import { observable } from "@trpc/server/observable";
+import type { RequestInitEsque } from "@trpc/client/dist/internals/types";
 import { createTRPCProxyClient, httpLink, type TRPCLink } from "@trpc/client";
 
 const errorLink: TRPCLink<AppRouter> = () => {
@@ -28,9 +29,28 @@ const trpc = createTRPCProxyClient<AppRouter>({
     errorLink,
     httpLink({
       url: "/api/trpc",
+      fetch: shimFetch,
     }),
   ],
   transformer: undefined,
 });
+
+function shimFetch(url: RequestInfo | URL, options?: RequestInit | RequestInitEsque) {
+  return fetch(url, {
+    ...options,
+    credentials: "include",
+  }).then((r) => {
+    if (r.redirected && r.url.includes("/.auth/login")) {
+      const url = new URL(r.url);
+
+      if (url.searchParams.get("post_login_redirect_uri")?.includes("/api")) {
+        url.searchParams.set("post_login_redirect_uri", "/");
+      }
+
+      window.location.href = url.href;
+    }
+    return r;
+  });
+}
 
 export default trpc;
